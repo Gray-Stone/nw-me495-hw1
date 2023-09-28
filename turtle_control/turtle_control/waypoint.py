1a459674a705cd8e8e4919e58dd9da1343fe9702
leo-chen <S8O9R6@u.northwestern.edu>
2023-09-28 10:43:16 -0500

    Minimal node created with timer

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -1,5 +1,37 @@
-def main():
-    print('Hi from turtle_control_try.')
+'''
+Control the turtle to move through a bunch of waypoints. 
+
+TODO(LEO) Finish later
+'''
+
+from typing import List, Optional
+import rclpy
+from rclpy.node import Node as RosNode
+
+
+def main(args=None):
+    '''
+    The main entry point. This joint entry method for both ros2 run through setup.py as well as direct file executaion 
+    '''
+    # This allows verifying the entry point
+    print(f'Starting at main of turtle control, given args: {args}')
+    rclpy.init(args=args)
+    waypoint_node = WaypointNode()
+
+    rclpy.spin(waypoint_node)
+    rclpy.shutdown()
+
+    
+
+
+class WaypointNode(RosNode):
+
+    def __init__(self) -> None:
+        super().__init__("waypoint")
+        self.timer = self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self) -> None:
+        self.get_logger().info("Timer_callback gets used")
 
 
 if __name__ == '__main__':
