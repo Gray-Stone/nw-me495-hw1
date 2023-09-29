da5d2784df93f62b6a32c16229a8de7357aa5fe5
leo-chen <S8O9R6@u.northwestern.edu>
2023-09-29 17:38:27 -0500

    Toggle service working

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -21,18 +21,38 @@ def main(args=None):
     rclpy.spin(waypoint_node)
     rclpy.shutdown()
 
-    
-
 
 class WaypointNode(RosNode):
+    '''
+    TODO(LEO)
+    '''
+
+    DEFAULT_TIMER_FREQUENCY :float = 100 # 100hz
+
+    class State(enum.Enum):
+        '''Internal state of the waypoint node
+        '''
+        STOPPED = 0
+        MOVING = 1
 
     def __init__(self) -> None:
         super().__init__("waypoint")
-        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.toggle_srv = self.create_service(std_srvs.srv.Empty ,"toggle",self.toggle_srv_callback)
 
     def timer_callback(self) -> None:
-        self.get_logger().info("Timer_callback gets used")
+        if self.state == self.State.MOVING:
+            self.get_logger().debug("Issuing Command")
 
+    def toggle_srv_callback(self ,_ :std_srvs.srv.Empty.Request , response:std_srvs.srv.Empty.Response) -> std_srvs.srv.Empty.Response :
+        # TODO(LEO) remove later
+        print("toggel service called")
 
+        if self.state == self.State.MOVING:
+            self.state = self.State.STOPPED
+            self.get_logger().info("Stopping")
+        else:
+            self.state = self.State.MOVING
+            self.get_logger().debug("Switching into moving state ")
+        return response
 if __name__ == '__main__':
     main()
