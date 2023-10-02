6072758c1bc6145ba4d303dfd4dec8e0eaad6463
leo-chen <S8O9R6@u.northwestern.edu>
2023-10-02 16:42:17 -0500

    Fix the timer frequency param part. (read combinging previous commit)

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -7,17 +7,21 @@ TODO(LEO) Finish later
 from typing import List, Optional
 import rclpy
 from rclpy.node import Node as RosNode
+import std_srvs.srv
+import enum
+
+from rcl_interfaces.msg import ParameterDescriptor as RosParameterDescriptor
 
 
 def main(args=None):
     '''
-    The main entry point. This joint entry method for both ros2 run through setup.py as well as direct file executaion 
+    The main entry point. This combines entry method for both ros2 run through
+    setup.py as well as direct file executaion 
     '''
     # This allows verifying the entry point
     print(f'Starting at main of turtle control, given args: {args}')
     rclpy.init(args=args)
     waypoint_node = WaypointNode()
-
     rclpy.spin(waypoint_node)
     rclpy.shutdown()
 
@@ -27,7 +31,7 @@ class WaypointNode(RosNode):
     TODO(LEO)
     '''
 
-    DEFAULT_TIMER_FREQUENCY :float = 100 # 100hz
+    DEFAULT_TIMER_FREQUENCY: float = 100.0  # 100hz
 
     class State(enum.Enum):
         '''Internal state of the waypoint node
@@ -37,13 +41,32 @@ class WaypointNode(RosNode):
 
     def __init__(self) -> None:
         super().__init__("waypoint")
-        self.toggle_srv = self.create_service(std_srvs.srv.Empty ,"toggle",self.toggle_srv_callback)
+
+        # Interal variables
+        self.state = self.State.STOPPED
+
+        # ROS stuff
+        self.declare_parameter(
+            "frequency", self.DEFAULT_TIMER_FREQUENCY,
+            RosParameterDescriptor(
+                description="The frequency command is issuing to turtles"))
+
+        self.timer_frequency = self.get_parameter(
+            "frequency").get_parameter_value().double_value
+
+        self.timer = self.create_timer(1.0 / self.timer_frequency,
+                                       self.timer_callback)
+        self.toggle_srv = self.create_service(std_srvs.srv.Empty, "toggle",
+                                              self.toggle_srv_callback)
 
     def timer_callback(self) -> None:
         if self.state == self.State.MOVING:
             self.get_logger().debug("Issuing Command")
 
-    def toggle_srv_callback(self ,_ :std_srvs.srv.Empty.Request , response:std_srvs.srv.Empty.Response) -> std_srvs.srv.Empty.Response :
+    def toggle_srv_callback(
+            self, _: std_srvs.srv.Empty.Request,
+            response: std_srvs.srv.Empty.Response
+    ) -> std_srvs.srv.Empty.Response:
         # TODO(LEO) remove later
         print("toggel service called")
 
@@ -54,5 +77,7 @@ class WaypointNode(RosNode):
             self.state = self.State.MOVING
             self.get_logger().debug("Switching into moving state ")
         return response
+
+
 if __name__ == '__main__':
     main()
