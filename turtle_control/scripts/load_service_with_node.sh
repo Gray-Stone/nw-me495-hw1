bd1589d4a59dc86cfcd4ead7d22550c00dad1281
leo-chen <S8O9R6@u.northwestern.edu>
2023-10-05 19:01:26 -0500

    Made the command issuing a lot more complicated, included toggle stuff

--- turtle_control/scripts/load_service_with_node.sh
+++ turtle_control/scripts/load_service_with_node.sh
@@ -7,15 +7,36 @@ readonly this="${this}"
 here="$(dirname "${this}")"
 readonly here="${here}"
 
-# We don't want to throw ros node in backgroun, Because it always cause issue. 
+# We don't want to throw ros node in backgroun, Because it always cause issue.
 # So the service call that's happening after node being created, will happen first.
-( 
-sleep 1 ;
-timeout -s TERM 1m ros2 service call /load turtle_interface/srv/Waypoints \
-'{waypoints: [{x: 2.0 , y: 1.0 , theta: 0.0}
+(
+    set -euvx
+
+    sleep 0.5
+    timeout -s TERM 1m ros2 service call /toggle std_srvs/srv/Empty
+    sleep 1
+
+    timeout -s TERM 1m ros2 service call /load turtle_interface/srv/Waypoints \
+        '{waypoints: [{x: 2.0 , y: 1.0 , theta: 0.0}
  , {x: 3.0 , y: 9.0 , theta: 0.8}
- , {x: 7.0 , y: 3.0 , theta: 0.8} ,{x: 5.0 , y: 6.0 , theta: 0.0}
  , {x: 10.0 , y: 6.0 , theta: 2.8}]}'
+
+    sleep 0.5
+    timeout -s TERM 1m ros2 service call /toggle std_srvs/srv/Empty
+
+    sleep 9
+    echo "ROS terminal call"
+    timeout -s TERM 1m ros2 service call /load turtle_interface/srv/Waypoints \
+        '{waypoints: [{x: 2.0 , y: 1.0 , theta: 0.0}
+ , {x: 3.0 , y: 9.0 , theta: 0.8}
+ , {x: 10.0 , y: 6.0 , theta: 2.8}]}'
+
+    while true; do
+        sleep 1
+        timeout -s TERM 1m ros2 service call /toggle std_srvs/srv/Empty
+        sleep 1
+        timeout -s TERM 1m ros2 service call /toggle std_srvs/srv/Empty
+    done
 ) &
 SRV_CALL_PID=$!
 
@@ -26,22 +47,21 @@ SRV_CALL_PID=$!
 
 trap -l
 
-
 cleanup() {
-echo "Clean up: killing other bg processes"
+    echo "Clean up: killing other bg processes"
 
-kill -TERM ${SRV_CALL_PID}
-kill -TERM ${TURTLE_PID}
+    kill -TERM ${SRV_CALL_PID}
+    kill -TERM ${TURTLE_PID}
 
-echo "Everythign should be killed now"
+    echo "Everythign should be killed now"
 
-ps ${SRV_CALL_PID}
-ps ${TURTLE_PID}
+    ps ${SRV_CALL_PID}
+    ps ${TURTLE_PID}
 }
 
 trap cleanup EXIT SIGINT
 # trap cleanup SIGINT
 
-"${here}/../turtle_control/waypoint.py" 
+"${here}/../turtle_control/waypoint.py"
 # "${here}/../turtle_control/waypoint.py" --ros-args --log-level debug
 exit 0
